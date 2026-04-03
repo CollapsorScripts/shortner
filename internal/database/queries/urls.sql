8b691b4306488f9267684172c793568312d03b4e
Collapsor <sahsa.c6@gmail.com>
2026-04-03 12:26:17 +0300

    добавлены запросы таблицы urls

--- internal/database/queries/urls.sql
+++ internal/database/queries/urls.sql
@@ -7,3 +7,14 @@ select original_url from urls where short_url = $1;
 -- name: GetOriginalUrlById :one
 select original_url from urls where id = $1;
 
+-- name: GetUrls :many
+select * from urls order by id;
+
+-- name: ListUrls :many
+select * from urls limit $1 offset $2 order by id;
+
+-- name: DeleteUrlById :exec
+delete from urls where id = $1;
+
+-- name: DeleteUrlByShortUrl :exec
+delete from urls where short_url = $1;
